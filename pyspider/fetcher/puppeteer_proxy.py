bf08d6fece330c47bd6666cb37d7482ef4fdaa57
kiwi <242637385@qq.com>
2019-04-15 07:29:54 +0800

    新分支

--- pyspider/fetcher/puppeteer_proxy.py
+++ pyspider/fetcher/puppeteer_proxy.py
@@ -67,7 +67,7 @@ class ForwordProxy():
         finally:
             local_writer.close()
     def start(self):
-        coro = asyncio.start_server(self.handle_client, '127.0.0.1', self.port,loop=self.loop)
+        coro = asyncio.start_server(self.handle_client, '127.0.0.1', self.port,loop=self.loop,backlog=5000)
         #asyncio.ensure_future(coro)
         self.loop.run_until_complete(coro)
 
