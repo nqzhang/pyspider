e4404c64eb5b9fdf00bd7967c8cd98f723d12d32
kiwi <242637385@qq.com>
2019-04-15 09:39:31 +0800

    新分支

--- pyspider/fetcher/puppeteer_fetcher.js
+++ pyspider/fetcher/puppeteer_fetcher.js
@@ -24,7 +24,7 @@ app.use(async (req, res, next) => {
         }
         */
         browser_settings["args"] = ['--no-sandbox', "--disable-setuid-sandbox","--user-data-dir=/dev/shm/puppeteer", "--proxy-server=127.0.0.1:8888"];
-        browser_settings["headless"] = false
+        browser_settings["headless"] = true
         browser = await puppeteer.launch(browser_settings);
         init_browser=false;
         console.log("init browser success!");
